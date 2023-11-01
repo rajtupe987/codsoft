18aefbe5e3e6284e95ba0b41a6eb786157c11b57
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-11-01 18:56:03 +0530

    done with calculator and todolist

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,36 +1,22 @@
-let currentInput = '';
-let firstOperand = null;
-let operator = null;
-let resultDisplayed = false;
 
 const display = document.getElementById('display');
 
-const buttons = document.querySelectorAll('.new, .oprator');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        handleButton(button.id);
-    });
-});
 
-function handleButton(value) {
-    if (value >= '0' && value <= '9') {
-        if (resultDisplayed) {
-            clearDisplay();
-            resultDisplayed = false;
-        }
-        currentInput += value;
-        updateDisplay();
-    } else if (value === 'C') {
-        clearAll();
-    } else if ('+-*/'.includes(value)) {
-        if (operator !== null) {
-            performCalculation();
-        }
-        operator = value;
-        firstOperand = currentInput;
-        clearDisplay();
-    } else if (value === 'Enter') {
-        performCalculation();
+
+
+function AppendToDisplay(value){
+    document.querySelector("#display").textContent+=value
+}
+
+function calculate() {
+ 
+    const expression=document.querySelector("#display").textContent;
+
+    try {
+        const result=eval(expression);
+        document.querySelector("#display").textContent=result;
+    } catch (error) {
+        document.querySelector("#display").textContent="Error"
     }
 }
 
@@ -39,8 +25,7 @@ function updateDisplay() {
 }
 
 function clearDisplay() {
-    currentInput = '';
-    updateDisplay();
+    document.querySelector("#display").textContent=""
 }
 
 function clearAll() {
@@ -50,38 +35,3 @@ function clearAll() {
     updateDisplay();
 }
 
-function performCalculation() {
-    if (firstOperand === null || operator === null) {
-        return;
-    }
-    const secondOperand = currentInput;
-    const a = parseFloat(firstOperand);
-    const b = parseFloat(secondOperand);
-    let result = 0;
-
-    switch (operator) {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            if (b !== 0) {
-                result = a / b;
-            } else {
-                result = 'Error';
-            }
-            break;
-    }
-
-    clearDisplay();
-    display.textContent = result;
-    resultDisplayed = true;
-    currentInput = result.toString();
-    operator = null;
-    firstOperand = null;
-}
